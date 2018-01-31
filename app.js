e597dedd8cae7118ca54fb4507c06e14814fe54d
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-01-31 13:35:25 -0800

    working database push

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyASzgtStABeTugbYGDLmZrAAXM-VREN944",
+  authDomain: "employee-data-management-31ec4.firebaseapp.com",
+  databaseURL: "https://employee-data-management-31ec4.firebaseio.com",
+  projectId: "employee-data-management-31ec4",
+  storageBucket: "employee-data-management-31ec4.appspot.com",
+  messagingSenderId: "838084561388"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $("#addButton").on("click", function(event) {
-	// Dynamically create the rows
+	event.preventDefault();
 	
+	var name = $("#name-input").val().trim();
+	var role = $("#role-input").val().trim();
+	var startDate = $("#start-date-input").val().trim();
+	var monthlyRate = $("#monthly-rate-input").val().trim();
+
+	console.log(name, role, startDate, monthlyRate);
+
+	var objectToPush = {
+		name: name,
+		role: role,
+		startDate: startDate,
+		monthlyRate: monthlyRate
+	}
+
+	database.ref().push(objectToPush);
+	// Dynamically create the rows
 });
+
